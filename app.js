354ea9cdf2b8b0aa46944c55fef0c85785b2ee91
Nolwenn <nolwenn.sparfel@gmail.com>
2025-01-28 14:59:50 +0100

    création de la page d'accueil, de la liste des problèmes et du formulaire d'ajout d'un problème

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-console.log('coucou');
+const express = require("express");
+const app = express();
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+
+let issues = [
+  {
+    auteur: "SamSoule",
+    titre: "Problème avec EJS",
+    description: "ma vue EJS ne marche pas",
+    etat: "en cours"
+  },
+  {
+    auteur: "Bob",
+    titre: "Bug dans le formulaire",
+    description: "Le champ email ne fonctionne pas",
+    etat: "résolu"
+  }
+
+];
+
+app.get("/issues", (req, res) => {
+  res.render("issues", { issues });
+});
+
+app.get("/create", (req, res) => {
+  res.render("create");
+});
+
+app.post("/issues", (req, res) => {
+  const { auteur, titre, description, etat } = req.body;
+   issues.push({auteur, titre, description, etat});
+   res.redirect("/issues");
+});
+
+app.listen(3000);
