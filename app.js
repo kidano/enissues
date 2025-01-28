82769ee2eb5d7dc37acdc123924b2b37bde86532
Justine44444 <137166047+Justine44444@users.noreply.github.com>
2025-01-28 14:41:24 +0100

    dossier_EJS

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('coucou');
+const express = require("express");
+const app = express();
+
+const port = 3000;
+app.set("views", "./views");
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+// route GET pour afficher la page d'accueil
+
+let donnees = [];
+app.get("/", (req, res) => {
+    res.render("accueil", {donnees});
+});
+
+app.post("/create", (req, res) => {
+    
+});
+
+app.listen(port, () => {
+  console.log("Le serveur tourne sur le port " + port);
+});
